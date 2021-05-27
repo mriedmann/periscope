4bd8ede5fec225a445b54695924201af2a33e53d
Michael Riedmann <michael_riedmann@live.com>
2021-05-28 01:44:20 +0200

    added continous run and prometheus metrics

--- periscope/cli.py
+++ periscope/cli.py
@@ -46,7 +46,12 @@ def parse_args(args=None):
     parser.add_argument("-i", "--interval",
                         nargs='?',
                         default=5,
-                        help="don't exit but repeat checks in given interval")
+                        help="don't exit but repeat checks in given interval. Also activates prometheus exporter")
+    
+    parser.add_argument("-p", "--port",
+                        nargs='?',
+                        default=9000,
+                        help="promtheus exporter port")
 
     for check in checks:
         parser.add_argument('--%s' % checks[check]['f'].__name__, nargs='*', help=checks[check]['help'])
