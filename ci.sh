82ee43bfb2491b1445e5278bdb4a2b608f8d3ab5
Michael Riedmann <michael_riedmann@live.com>
2022-07-20 10:16:50 +0200

    ci: add 3.10 checks

--- ci.sh
+++ ci.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-for v in 3.7 3.8 3.9; do
+for v in 3.7 3.8 3.9 3.10; do
     podman build -f Dockerfile.ci --build-arg python_version=$v -t pipecheck-ci:$v .
     podman run --rm -t --name pipecheck-ci-$v -v "$PWD":/usr/src/app:z pipecheck-ci:$v
 done
